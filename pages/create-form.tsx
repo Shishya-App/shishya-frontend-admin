041331d8252707aec44b812bf1d52a96db3bc70c
Madhur Gupta <madhurgupta107@gmail.com>
2022-08-24 21:04:09 +0530

    Adds checkbox on create form page

--- pages/create-form.tsx
+++ pages/create-form.tsx
@@ -1,73 +1,55 @@
-import React from "react";
-import styles from "../styles/Pages/create-form.module.css";
-import CreateFormStatus from "../components/CreateFormStatus";
-import CustomButton from "../components/CustomButton";
+import React from 'react'
+import styles from '../styles/Pages/create-form.module.css'
+import CreateFormStatus from '../components/CreateFormStatus'
+import CustomButton from '../components/CustomButton'
 
-import CreateFormStep1 from "../components/create-form/create-form-step-1";
-import CreateFormStep2 from "../components/create-form/create-form-step-2";
-import CreateFormStep3 from "../components/create-form/create-form-step-3";
-
+import CreateFormStep1 from '../components/create-form/create-form-step-1'
+import CreateFormStep2 from '../components/create-form/create-form-step-2'
+import CreateFormStep3 from '../components/create-form/create-form-step-3'
 
 const CreateForm = () => {
-    const [step, setStep] = React.useState(1);
-    // const academicYearData = [
-    //     {
-    //         value: "2020/24",
-    //         name: "2020/24"
-    //     }, 
-    //     {
-    //         value: "2021/25",
-    //         name: "2021/25"
-    //     },
-    //     {
-    //         value: "2022/26",
-    //         name: "2022/26"
-    //     },
-    //     {
-    //         value: "2023/27",
-    //         name: "2023/27"
-    //     }
-    // ]
+	const [step, setStep] = React.useState(1)
 
-    const handleStep = () => {
-        switch(step){
-            case 1:
-                return <CreateFormStep1 />
-            case 2:
-                return <CreateFormStep2 />
-            case 3:
-                return <CreateFormStep3 />
-            default:
-                return null
-        }
-    }
+	const handleStep = () => {
+		switch (step) {
+			case 1:
+				return <CreateFormStep1 />
+			case 2:
+				return <CreateFormStep2 />
+			case 3:
+				return <CreateFormStep3 />
+			default:
+				return null
+		}
+	}
 
-    return (
-        <div className={styles.form__components}>
-            <p className={styles.create__form__title}>Create New Form</p>
-            <div className={styles.create__form__status__wrap}>
-                <CreateFormStatus />
-            </div>
-            {handleStep()}
-            <div className={styles.button__wrapper}>
-            
-            <div className={styles.button__internal__wrapper}>
-                {
-                    step > 1 ?
-                    <CustomButton handleClick={() => setStep(step-1)} title={"Back"}/>
-                    : null
-                }
-                </div>
-                <div className={styles.button__internal__wrapper}>
-                    {
-                        step < 3 ?
-                        <CustomButton handleClick={() => setStep(step+1)} title={"Next"}/>
-                        : null
-                    }
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.form__components}>
+			<p className={styles.create__form__title}>Create New Form</p>
+			<div className={styles.create__form__status__wrap}>
+				<CreateFormStatus />
+			</div>
+			{handleStep()}
+			<div className={styles.button__wrapper}>
+				<div className={styles.button__internal__wrapper}>
+					{step > 1 ? (
+						<CustomButton
+							handleClick={() => setStep(step - 1)}
+							title={'Back'}
+						/>
+					) : null}
+				</div>
+				<div className={styles.button__internal__wrapper}>
+					{step < 3 ? (
+						<CustomButton
+							handleClick={() => setStep((prevStep) => prevStep + 1)}
+							title={'Next'}
+						/>
+					) : null}
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default CreateForm;
+export default CreateForm
